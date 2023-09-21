7b5cad0d5440665a8d57cefc0b4255d6f1aba0db
snusEbjoer <pojiratelsnusa@gmail.com>
2023-09-22 00:40:51 +0300

    fixed some ooopsy doopsy

--- src/components/CreatePost.tsx
+++ src/components/CreatePost.tsx
@@ -40,8 +40,8 @@ export const CreatePost: React.FC<CreatePostProps> = ({ setPosts }) => {
     return (
         <div className='flex flex-row p-4 justify-center'>
             <div className='flex flex-col justify-center w-1/2'>
-                <input placeholder='Title' className='border-0 focus:outline-none text-5xl mb-4 font-light resize-none placeholder:text-purple-300 break-all h-fit  overflow-hidden' required name='title' value={post.title} onChange={handleChange} />
-                <textarea placeholder='Content' className='border-0 focus:outline-none h-full text-4xl resize-y font-extralight placeholder:text-purple-200 break-all h-fit  overflow-hidden' required name='content' value={post.content} onChange={handleChange} />
+                <textarea placeholder='Title' className='border-0 focus:outline-none text-5xl mb-4 font-light resize-none placeholder:text-purple-300 break-all h-fit overflow-hidden' required name='title' value={post.title} onChange={handleChange} />
+                <textarea placeholder='Content' className='border-0 focus:outline-none h-full text-4xl resize-none font-extralight placeholder:text-purple-200 break-all h-fit  overflow-hidden' required name='content' value={post.content} onChange={handleChange} />
                 <button className='text-white bg-gradient-to-r from-purple-500 to-pink-500 hover:bg-gradient-to-l focus:ring-4 focus:outline-none focus:ring-purple-200 dark:focus:ring-purple-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center' onClick={handleSubmit}>Publish</button>
             </div >
         </div >
--- src/components/PostComp.tsx
+++ src/components/PostComp.tsx
@@ -1,6 +1,6 @@
 import axios from "axios"
 import { Post, PostProps } from "../types"
-import { useState } from "react"
+import React, { useState } from "react"
 
 
 
@@ -8,6 +8,8 @@ import { useState } from "react"
 export const PostComp: React.FC<PostProps> = ({ post, setPosts }) => {
 
     const [updateFLag, setUpdateFlag] = useState<boolean>(false)
+    const [title, setTitle] = useState<string>(post.title)
+    const [content, setContent] = useState<string>(post.content)
 
     const deletePost = (post: Post) => {
         axios.delete(`http://127.0.0.1:5000/posts/${post.id}`).then((res) => {
@@ -39,25 +41,31 @@ export const PostComp: React.FC<PostProps> = ({ post, setPosts }) => {
         setUpdateFlag((prev: boolean) => !prev)
     }
 
+    const handleChange = (func: React.Dispatch<React.SetStateAction<string>>, e: React.ChangeEvent<HTMLTextAreaElement>) => {
+        func(e.target.value)
+    }
+
 
     return (
-        <div className="container mx-auto flex justify-center flex-col">
+        <div className="container mx-auto flex justify-center items-center flex-col">
 
-            <form onSubmit={handleSubmit} className="flex flex-row justify-center">
+            <form onSubmit={handleSubmit} className="flex flex-col justify-center w-1/2">
                 <div className='max-w-md mt-4 mr-4'>
-                    {updateFLag ?
-                        <button className='text-white bg-gradient-to-r from-purple-500 to-pink-500 hover:bg-gradient-to-l focus:ring-4 focus:outline-none focus:ring-purple-200 dark:focus:ring-purple-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center' type="submit">Publish</button> :
-                        <>
-                            <button className="text-white bg-gradient-to-r from-purple-500 to-pink-500 hover:bg-gradient-to-l focus:ring-4 focus:outline-none focus:ring-purple-200 dark:focus:ring-purple-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mb-2 mr-2" type="button" onClick={toggleUpdate}>Update</button>
-                            <button className="text-white bg-gradient-to-r from-purple-500 to-pink-500 hover:bg-gradient-to-l focus:ring-4 focus:outline-none focus:ring-purple-200 dark:focus:ring-purple-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mb-2 mr-4" type="button" onClick={() => deletePost(post)}>Delete</button>
-                        </>
-                    }
                 </div>
                 <div className='flex flex-col justify-center w-1/2'>
-                    <textarea name="title" placeholder='Title' className='border-0 focus:outline-none text-5xl mb-4 font-light resize-none placeholder:text-purple-300 break-all h-fit  overflow-hidden' readOnly={!updateFLag} rows={2} value={post.title} />
-                    <textarea name="content" placeholder='Content' className='border-0 focus:outline-none h-full text-4xl resize-none font-extralight placeholder:text-purple-200 break-all h-fit  overflow-hidden' readOnly={!updateFLag} rows={15} value={post.content} />
+                    <textarea name="title" placeholder='Title' className='border-0 focus:outline-none text-5xl mb-4 font-light resize-none placeholder:text-purple-300 break-all h-fit  overflow-hidden' readOnly={!updateFLag} value={title} onChange={(e) => handleChange(setTitle, e)} />
+                    <textarea name="content" placeholder='Content' className='border-0 focus:outline-none h-full text-4xl resize-none font-extralight placeholder:text-purple-200 break-all h-fit  overflow-hidden' readOnly={!updateFLag} value={content} onChange={(e) => handleChange(setContent, e)} />
                 </div >
+                {updateFLag ?
+                    <div>
+                        <button className='text-white bg-gradient-to-r from-purple-500 to-pink-500 hover:bg-gradient-to-l focus:ring-4 focus:outline-none focus:ring-purple-200 dark:focus:ring-purple-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center' key={1} type="submit">Publish</button>
+                    </div> :
+                    <div className="flex flex-row items-center justify-center grow">
+                        <button className="grow text-white bg-gradient-to-r from-purple-500 to-pink-500 hover:bg-gradient-to-l focus:ring-4 focus:outline-none focus:ring-purple-200 dark:focus:ring-purple-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mb-2 mr-2" key={2} type="button" onClick={toggleUpdate}>Update</button>
+                        <button className=" grow text-white bg-gradient-to-r from-purple-500 to-pink-500 hover:bg-gradient-to-l focus:ring-4 focus:outline-none focus:ring-purple-200 dark:focus:ring-purple-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mb-2 mr-4" key={3} type="button" onClick={() => deletePost(post)}>Delete</button>
+                    </div>
 
+                }
             </form>
 
         </div>
